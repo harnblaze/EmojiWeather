2b539db24e6ebb7935dfd88002f1da3577568682
Dmitriy Emelyanov <hanrblaze@gmail.com>
2025-10-03 21:52:38 +0300

    Поправил eslint.config

--- EmojiWeather/eslint.config.js
+++ EmojiWeather/eslint.config.js
@@ -15,7 +15,6 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      'plugin:prettier/recommended',
     ],
     languageOptions: {
       ecmaVersion: 2020,
