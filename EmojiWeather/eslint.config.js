5b9bd5376556e81f75a3426e93321cb3934ea9f2
Dmitriy Emelyanov <hanrblaze@gmail.com>
2025-10-03 22:02:29 +0300

    Еще раз поправил eslint.config

--- EmojiWeather/eslint.config.js
+++ EmojiWeather/eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,7 +21,7 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: { prettier },
+    plugins: { prettier, eslintConfigPrettier },
     rules: {
       'prettier/prettier': 'warn',
     },
